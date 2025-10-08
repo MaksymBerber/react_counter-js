ea919550468970189647343f77f3fd2e10f98c39
MaksymBerber <marcusbmmb@outlook.com>
2025-10-08 19:09:26 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(100);
 
   const addOne = () => {
     setCount(prevCount => prevCount + 1);
